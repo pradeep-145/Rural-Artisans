c7e0772867a960922bf2422252ee29eb21dd71b9
Nikilhari <nikilhari3@gmail.com>
2025-02-21 21:19:42 +0530

    nav changes

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { IoCartOutline } from "react-icons/io5";
+// import { IoCartOutline } from "react-icons/io5";
 import { GiThreeLeaves } from "react-icons/gi";
 import { GrCart } from "react-icons/gr";
 import { FaRegUser, FaSearch, FaRegHeart } from "react-icons/fa";
@@ -7,7 +7,7 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const [login, setLogin] = useState(null);
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  // const [isCartOpen, setIsCartOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
@@ -35,11 +35,20 @@ const Navbar = () => {
         </div>
 
         <div className={styles.usernav__contents}>
-          <div className={styles.usernav__items}>HOME</div>
-          <div className={styles.usernav__items}>PRODUCT</div>
-          <div className={styles.usernav__items}>ABOUT</div>
-          <div className={styles.usernav__items}>TESTIMONIALS</div>
-          <div className={styles.usernav__items}>CONTACT</div>
+          {login === "customer" ? (
+            <>
+              <div className={styles.usernav__items}>HOME</div>
+              <div className={styles.usernav__items}>PRODUCT</div>
+              <div className={styles.usernav__items}>ABOUT</div>
+              <div className={styles.usernav__items}>TESTIMONIALS</div>
+              <div className={styles.usernav__items}>CONTACT</div>
+            </>
+          ) : (
+            <>
+              <div className={styles.usernav__items}>HOME</div>
+              <div className={styles.usernav__items}>VIEW ORDERS</div>
+            </>
+          )}
         </div>
 
         <div className={styles.search__box}>
@@ -56,7 +65,7 @@ const Navbar = () => {
           <button className={styles.auth__button} onClick={handleAuth}>
             Login / Register
           </button>
-        ) : (
+        ) : login == "customer" ? (
           <div className={styles.auth__cart_container}>
             <FaRegUser className={styles.__icon} onClick={handleAuth} />
 
@@ -65,6 +74,8 @@ const Navbar = () => {
             </div>
             <FaRegHeart className={styles.__icon} />
           </div>
+        ) : (
+          <FaRegUser className={styles.__icon} onClick={handleAuth} />
         )}
       </div>
     </nav>
